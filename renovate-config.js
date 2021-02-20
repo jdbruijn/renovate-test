c8beada67a20f10a44e40a361d2e941775f9be66
Jeroen de Bruijn <vv9239@gmail.com>
2021-02-20 10:41:40 +0100

    chore: run on jdbruijn/homebrew-caboodle

--- renovate-config.js
+++ renovate-config.js
@@ -4,15 +4,13 @@ module.exports = {
     branchPrefix: `${branchName}/`,
     dependencyDashboardTitle: 'Dependency Dashboard self-hosted',
     gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
-    logLevel: 'debug',
     onboarding: true,
     onboardingBranch: `${branchName}/configure`,
     platform: 'github',
-    dryRun: true,
+    dryRun: false,
     username: 'jdbruijn',
     repositories: [
         'jdbruijn/homebrew-caboodle',
-        'jdbruijn/.github',
     ],
 };
 
